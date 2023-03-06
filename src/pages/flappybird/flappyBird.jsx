48008d168465668535232e18d182d566e0a4e2ad
Ryland Adams <rylandadams@Rylands-MacBook-Pro.local>
2023-03-05 23:31:31 -0800

    end if night

--- src/pages/flappybird/flappyBird.jsx
+++ src/pages/flappybird/flappyBird.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import './flappyBird.css';
 
-import GameImport from './gameImport';
-
 const Flappybird = () => {
 	// -------------------------------------------
-	const game = GameImport('./index.html');
+
 	// -------------------------------------------
 
-	return <div className='game'>{game}</div>;
+	return <div className='game'></div>;
 };
 
 export default Flappybird;
