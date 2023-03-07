cc586750843bc342063de90f7ef4ae4943aa67f7
Ryland Adams <rylandadams@Rylands-MacBook-Pro.local>
2023-03-06 16:20:56 -0800

    iframe flappy

--- src/pages/flappybird/flappyBird.jsx
+++ src/pages/flappybird/flappyBird.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import './flappyBird.css';
+import Iframe from 'react-iframe';
 
 const Flappybird = () => {
-	return <div className='game'></div>;
+	return (
+		<div className='game'>
+			<Iframe
+				scrolling='no'
+				url='https://playcanv.as/p/2OlkUaxF/'
+				width='130%'
+				height='120%'
+				// id=''
+				// className=''
+				// display='block'
+				// position='relative'
+			/>
+		</div>
+	);
 };
 
 export default Flappybird;
