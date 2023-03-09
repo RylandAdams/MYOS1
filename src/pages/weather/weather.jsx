25b7886180af73953d07b94d10cbf66d8147862a
Ryland Adams <rylandadams@Rylands-MacBook-Pro.local>
2023-03-08 23:06:11 -0800

    heatwave starting

--- src/pages/weather/weather.jsx
+++ src/pages/weather/weather.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import Iframe from 'react-iframe';
 import './weather.css';
 
 const Weather = () => {
-	return <div className='weatherPage'>Weather</div>;
+	return (
+		<div className='weatherPage'>
+			<div>
+				<Iframe
+					className='spongebob'
+					src='https://gifer.com/embed/3x'
+					// frameBorder='1'
+				></Iframe>
+			</div>
+			<p>
+				<a href='https://gifer.com'></a>
+			</p>
+		</div>
+	);
 };
 
 export default Weather;
