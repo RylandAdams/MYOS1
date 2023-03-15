c9687d8038cfe1a7989d0f6a473b980fb039335a
Ryland <rylandadams@yahoo.com>
2023-03-15 11:49:38 -0700

    headers for messages styled for smaller screens

--- src/components/topBar/topBar.jsx
+++ src/components/topBar/topBar.jsx
@@ -4,7 +4,7 @@ import './topBar.css';
 
 import BatteryGauge from 'react-battery-gauge';
 import { GiNetworkBars } from 'react-icons/gi';
-import { CgBatteryFull } from 'react-icons/cg';
+import { FaBatteryFull } from 'react-icons/fa';
 
 const TopBar = () => {
 	const [date, setDate] = useState(new Date());
@@ -47,7 +47,10 @@ const TopBar = () => {
 				})}
 			</div>
 			<div className={location.pathname === '/' ? 'right' : 'rightAlt'}>
-				<CgBatteryFull />
+				<FaBatteryFull
+					color={location.pathname === '/' ? '#d2d2d2' : '#000000'}
+					fill={location.pathname === '/' ? '#d2d2d2' : '#000000'}
+				/>
 			</div>
 		</div>
 	);
