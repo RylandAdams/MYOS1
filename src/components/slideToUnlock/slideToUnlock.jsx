cf9113332aa51aa36ddf9df92073d35167470b47
Ryland <rylandadams@yahoo.com>
2023-06-29 18:52:42 -0700

    slider progress

--- src/components/slideToUnlock/slideToUnlock.jsx
+++ src/components/slideToUnlock/slideToUnlock.jsx
@@ -1,72 +1,109 @@
-import React, { useEffect } from 'react';
-import $ from 'jquery';
-import 'jquery-ui-dist/jquery-ui';
+import React, { useEffect, useRef } from 'react';
 import './slideToUnlock.css';
 
 const SlideToUnlock = () => {
+	const sliderRef = useRef(null);
+	const arrowRef = useRef(null);
+	const wellRef = useRef(null);
+
 	useEffect(() => {
-		$(function () {
-			$('#slider').draggable({
-				axis: 'x',
-				containment: 'parent',
-				drag: function (event, ui) {
-					if (ui.position.left > 550) {
-						$('#well').fadeOut();
-					} else {
-						// Apparently Safari isn't allowing partial opacity on text with background clip? Not sure.
-						// $("h2 span").css("opacity", 100 - (ui.position.left / 5))
-					}
-				},
-				stop: function (event, ui) {
-					if (ui.position.left < 551) {
-						$(this).animate({
-							left: 0,
-						});
-					}
-				},
-			});
+		const slider = sliderRef.current;
+		const arrow = arrowRef.current;
+		const well = wellRef.current;
 
-			$('#slider')[0].addEventListener(
-				'touchmove',
-				function (event) {
-					event.preventDefault();
-					var el = event.target;
-					var touch = event.touches[0];
-					var curX = touch.pageX - this.offsetLeft - 73;
-					if (curX <= 0) return;
-					if (curX > 550) {
-						$('#well').fadeOut();
-					}
-					el.style.webkitTransform = 'translateX(' + curX + 'px)';
-				},
-				false
-			);
+		let startX = 0;
+		let startTranslateX = 0;
+		let isDragging = false;
 
-			$('#slider')[0].addEventListener(
-				'touchend',
-				function (event) {
-					this.style.webkitTransition =
-						'-webkit-transform 0.3s ease-in';
-					this.addEventListener(
-						'webkitTransitionEnd',
-						function (event) {
-							this.style.webkitTransition = 'none';
-						},
-						false
-					);
-					this.style.webkitTransform = 'translateX(0px)';
-				},
-				false
-			);
-		});
+		const handleDragStart = (event) => {
+			if (event.touches) {
+				startX = event.touches[0].clientX;
+			} else {
+				startX = event.clientX;
+			}
+			startTranslateX = getTranslateX();
+			isDragging = true;
+		};
+
+		const handleDragMove = (event) => {
+			if (!isDragging) return;
+			event.preventDefault();
+
+			let clientX;
+			if (event.touches) {
+				clientX = event.touches[0].clientX;
+			} else {
+				clientX = event.clientX;
+			}
+
+			const diffX = clientX - startX;
+			const newTranslateX = startTranslateX + diffX;
+			setTranslateX(newTranslateX);
+		};
+
+		const handleDragEnd = () => {
+			isDragging = false;
+			const translateX = getTranslateX();
+			if (translateX > 550) {
+				well.style.display = 'none';
+			} else {
+				setTranslateX(0);
+			}
+		};
+
+		const getTranslateX = () => {
+			const transformValue = slider.style.transform;
+			if (!transformValue) return 0;
+			const match = transformValue.match(/translateX\((.*?)px\)/);
+			if (!match || !match[1]) return 0;
+			return parseInt(match[1], 10);
+		};
+
+		const setTranslateX = (translateX) => {
+			slider.style.transform = `translateX(${translateX}px)`;
+			arrow.style.transform = `translateX(${translateX}px)`;
+		};
+
+		// Add event listeners for touch events
+		slider.addEventListener('mousedown', handleDragStart);
+		slider.addEventListener('touchstart', handleDragStart);
+
+		document.addEventListener('mousemove', handleDragMove);
+		document.addEventListener('touchmove', handleDragMove);
+
+		document.addEventListener('mouseup', handleDragEnd);
+		document.addEventListener('touchend', handleDragEnd);
+
+		return () => {
+			// Clean up the event listeners
+			slider.removeEventListener('mousedown', handleDragStart);
+			slider.removeEventListener('touchstart', handleDragStart);
+
+			document.removeEventListener('mousemove', handleDragMove);
+			document.removeEventListener('touchmove', handleDragMove);
+
+			document.removeEventListener('mouseup', handleDragEnd);
+			document.removeEventListener('touchend', handleDragEnd);
+		};
 	}, []);
 
 	return (
 		<div id='page-wrap'>
-			<div id='well'>
+			<div
+				id='well'
+				ref={wellRef}
+			>
 				<h2>
-					<strong id='slider'></strong> <span>slide to unlock</span>
+					<strong
+						id='slider'
+						ref={sliderRef}
+					></strong>
+					<span>slide to unlock</span>
 				</h2>
+				<div
+					className='arrow'
+					ref={arrowRef}
+				></div>
 			</div>
 		</div>
 	);
