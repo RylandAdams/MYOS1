11b70f5edd8464ca4cb86674bf5a03665099a4bb
Ryland <rylandadams@yahoo.com>
2023-09-16 16:24:03 -0700

    little stuff

--- src/components/LockScreen/lockScreen.jsx
+++ src/components/LockScreen/lockScreen.jsx
@@ -1,11 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import './lockScreen.css';
-import { motion } from 'framer-motion';
+
 import SlideToUnlock from '../slideToUnlock/slideToUnlock';
 import lockScreenBackground from '../../assets/imgs/lockScreenBackground.jpg';
+import linkTreeImg from '../../assets/imgs/linkTree.png';
+
+import { motion, AnimatePresence } from 'framer-motion';
+
+let didInit = false;
 
 const LockScreen = () => {
 	const [date, setDate] = useState(new Date());
+	const [notification, setNotification] = useState(false);
+
+	// Initialize notification after 2 seconds
+	if (!didInit) {
+		setTimeout(() => {
+			setNotification(true);
+			shownotification();
+		}, 200);
+	}
+
+	// Show notification and hide after 3.45 seconds
+	const shownotification = () => {
+		didInit = true;
+		setTimeout(() => {
+			setNotification(false);
+		}, 34500);
+	};
 
 	function refreshClock() {
 		setDate(new Date());
@@ -63,6 +85,40 @@ const LockScreen = () => {
 				<div className='lockScreenDate'>{formattedDate}</div>
 				<div className='lockScreenBottomTransSqr'></div>
 			</motion.div>
+			{/* Render notification if notification state is true */}
+			{notification ? (
+				<motion.a
+					initial={{ opacity: 0, y: -10, zIndex: 20 }}
+					// exit={{ opacity: 0, y: -10, zIndex: 20 }}
+					animate={{
+						opacity: 1,
+						y: 0,
+						zIndex: 20,
+					}}
+					transition={{
+						duration: 1.5,
+						type: 'spring',
+						repeat: 1,
+						// repeatType: 'reverse',
+						// repeatDelay: 0.5,
+					}}
+					key='notification'
+					target='_blank'
+					onClick='https://linktr.ee/ry.land?utm_source=linktree_admin_share'
+					href='https://linktr.ee/ry.land?utm_source=linktree_admin_share'
+					className='notificationAlert'
+				>
+					<img
+						className='notificationImg'
+						src={linkTreeImg}
+						alt='BadWeather'
+					/>
+					<div className='notificationTextTop'>Bad Weather - EP</div>
+					<div className='notificationTextBottem'></div>
+				</motion.a>
+			) : (
+				''
+			)}
 		</>
 	);
 };
--- src/components/homeScreen/homeScreen.jsx
+++ src/components/homeScreen/homeScreen.jsx
@@ -1,76 +1,23 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './homeScreen.css';
 import BadWeather from '../../assets/songs/BadWeather.png';
+import homeScreenBackground from '../../assets/imgs/iosbackground.jpeg';
 
-import { motion, AnimatePresence } from 'framer-motion';
+import { motion } from 'framer-motion';
 
 import dock from '../../assets/imgs/DockBar.jpg';
 
 import App from '../app';
 import { MAINAPPS, FOOTERAPPS } from '../../assets/apps';
 
-let didInit = false;
-
 const HomeScreen = () => {
-	// State for notification
-	const [notification, setNotification] = useState(false);
-
-	// Initialize notification after 2 seconds
-	if (!didInit) {
-		setTimeout(() => {
-			setNotification(true);
-			shownotification();
-		}, 2000);
-	}
-
-	// Show notification and hide after 3.45 seconds
-	const shownotification = () => {
-		didInit = true;
-		setTimeout(() => {
-			setNotification(false);
-		}, 3450);
-	};
-
 	return (
 		<>
-			{/* Render notification if notification state is true */}
-			{notification ? (
-				<AnimatePresence>
-					<motion.a
-						initial={{ opacity: 0, y: -10, zIndex: 20 }}
-						exit={{ opacity: 0, y: -10, zIndex: 20 }}
-						animate={{
-							opacity: 1,
-							y: 0,
-							zIndex: 20,
-						}}
-						transition={{
-							duration: 1.5,
-							type: 'spring',
-							repeat: 1,
-							repeatType: 'reverse',
-							repeatDelay: 0.5,
-						}}
-						key='notification'
-						target='_blank'
-						onClick='https://distrokid.com/hyperfollow/ryland2/bad-weather---ep-2'
-						href='https://distrokid.com/hyperfollow/ryland2/bad-weather---ep-2'
-						className='notificationAlert'
-					>
-						<img
-							className='notificationImg'
-							src={BadWeather}
-							alt='BadWeather'
-						/>
-						<div className='notificationTextTop'>
-							Bad Weather - EP
-						</div>
-						<div className='notificationTextBottem'></div>
-					</motion.a>
-				</AnimatePresence>
-			) : (
-				''
-			)}
+			<img
+				src={homeScreenBackground}
+				alt=''
+				className='homeScreenPicture'
+			/>
 
 			{/* Render apps */}
 			<motion.div
